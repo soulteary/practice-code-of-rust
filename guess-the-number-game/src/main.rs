82b961859b6c27009de69851d59ab16be7ba8c89
soulteary <soulteary@gmail.com>
2021-02-27 20:20:53 +0800

    use error struct desc expect

--- guess-the-number-game/src/main.rs
+++ guess-the-number-game/src/main.rs
@@ -7,22 +7,42 @@ struct Range {
     max: u8,
 }
 
+struct Error {}
+
+impl Error {
+    fn show_question(turn: &u8, min: &u8, max: &u8) {
+        println!("[TURN #{}]Give me an answer? [{}, {}]\n", turn, min, max);
+    }
+    fn show_out_of_range(min: &u8, max: &u8) {
+        println!("> Should pick a number between [{}, {}]\n", min, max);
+    }
+    fn show_too_large() {
+        println!("> Should be less than the maximum.\n");
+    }
+    fn show_too_small() {
+        println!("> Should be greater than the minimum.\n");
+    }
+    fn show_less_than_answer() {
+        println!("> The value is too small, readjust the range.\n");
+    }
+    fn show_larger_than_answer() {
+        println!("> The value is too large, readjust the range.\n");
+    }
+}
+
 const RANGE_LIMIT: Range = Range { min: 1, max: 100 };
 
 fn main() {
-    let mut _min_number = RANGE_LIMIT.min;
-    let mut _max_number = RANGE_LIMIT.max;
-    let mut _turn = 1;
+    let mut _min_number: u8 = RANGE_LIMIT.min;
+    let mut _max_number: u8 = RANGE_LIMIT.max;
+    let mut _turn: u8 = 1;
 
     let _secret_number = rand::thread_rng().gen_range(RANGE_LIMIT.min, RANGE_LIMIT.max + 1);
 
     println!("[Guess Number]");
 
     loop {
-        println!(
-            "[TURN #{}]Give me an answer? [{}, {}]",
-            _turn, _min_number, _max_number
-        );
+        Error::show_question(&_turn, &_min_number, &_max_number);
         _turn += 1;
 
         let mut guess_number = String::new();
@@ -33,31 +53,27 @@ fn main() {
         let guess_number: u8 = match guess_number.trim().parse() {
             Ok(n) => n,
             Err(_) => {
-                println!(
-                    "> Should pick a number between [{}, {}]",
-                    _min_number, _max_number
-                );
+                Error::show_out_of_range(&_min_number, &_max_number);
                 continue;
             }
         };
-        println!("Your input: {}", guess_number);
-        println!("");
+        println!("Your input: {}\n", guess_number);
 
         if guess_number >= _max_number {
-            println!("> Should be less than the maximum.");
+            Error::show_too_large();
             continue;
         } else if guess_number <= _min_number {
-            println!("> Should be greater than the minimum.");
+            Error::show_too_small();
             continue;
         }
 
         match guess_number.cmp(&_secret_number) {
             Ordering::Less => {
-                println!("> The value is too small, readjust the range.\n");
+                Error::show_less_than_answer();
                 _min_number = guess_number;
             }
             Ordering::Greater => {
-                println!("> The value is too large, readjust the range.\n");
+                Error::show_larger_than_answer();
                 _max_number = guess_number;
             }
             Ordering::Equal => {
